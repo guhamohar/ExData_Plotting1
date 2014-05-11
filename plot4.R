6d26b55ea98b63ae923442ee56682666c1693610
Mohar <guhamohar@gmail.com>
2014-05-11 00:12:37 -0700

    changes to plot4

--- plot4.R
+++ plot4.R
@@ -18,14 +18,15 @@ datetime <- as.POSIXct(paste(dateMod, myData$Time), format="%Y-%m-%d %H:%M:%S")
 
 png(file="plot4.png",height=480, width=480,bg='transparent')
 par(mfrow = c(2,2))
-plot(datetime,myData$Global_active_power,type="l",xlab=" ",ylab="Global Active Power (kilowatts)")
+plot(datetime,myData$Global_active_power,type="l",xlab=" ",ylab="Global Active Power")
 plot(datetime,myData$Voltage,type="l",xlab="datetime ",ylab="Voltage")
 
+yr<-range(c(myData$Sub_metering_1,myData$Sub_metering_2,myData$Sub_metering_3))
 plot(range(datetime),yr,xlab=" ",ylab="Energy sub metering",type="n")
 lines(datetime,myData$Sub_metering_1,type='l',col='black')
 lines(datetime,myData$Sub_metering_2,type='l',col='red')
 lines(datetime,myData$Sub_metering_3,type='l',col='blue')
 legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="n")
 
-plot(datetime,myData$Global_reactive_power,type="l",pch=1,xlab="datetime ",ylab="Global_reactive_power")
+plot(datetime,myData$Global_reactive_power,type="l",pch=1,xlab="datetime ",ylab="Global_reactive_power",lwd=0.1)
 dev.off()
